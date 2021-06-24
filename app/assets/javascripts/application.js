ffccb0291fc413ec45f35bb9bcca06ba073ba92b
Angelo Igitego <angelo.igitego@gmail.com>
2021-06-24 23:29:30 +0200

    Attempt to fix datatable issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
+// require jquery
 //= require jquery3
+//= require jquery_ujs
+// require rails-ujs
+//= require turbolinks
 //= require moment
 //= require bootstrap-sprockets
 //= require bootstrap-datetimepicker
