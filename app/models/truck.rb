b823ec72f87f8fae82fcecdff0f8119e311f72b3
Angelo Igitego <angelo.igitego@gmail.com>
2021-07-05 12:38:10 +0200

    Validations for trucks

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,5 +1,7 @@
 class Truck < ApplicationRecord
 	has_many :containers, inverse_of: :truck
+	validates :unique_number, presence: true, uniqueness: true
+	validates :license_number, presence: true, uniqueness: true
 
 	def name 
 		self.unique_number	
